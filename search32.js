c07e14f23d474a1ef62ca54ab936deeeeebe5475
Shoyo772 <112268507+Shoyo772@users.noreply.github.com>
2022-08-27 18:56:48 +0200

    first uptade
    base du site

---
+++ search32.js
@@ -0,0 +1,122 @@
+const persons = [
+
+    {name:'Adobe Premiere Pro 2020',lien:'cracks/Adobe Premiere Pro 2020/Adobe Premiere Pro 2020 '},
+    {name:'Adobe After Effect 2020',lien:'cracks/Adobe After Effect 2020/Adobe After Effect 2020 '},
+    {name:'Attack on Titan 2',lien:'cracks/Attack on Titan 2/Attack on Titan 2 '},
+    {name:'BeamNG Drive',lien:'cracks/BeamNG%20Drive/BeamNG%20Drive '},
+    {name:'Bigfoot',lien:'cracks/Bigfoot/Bigfoot '},
+    {name:'Cinema4D',lien:'cracks/Cinema4D/Cinema4D '},
+    {name:'Captain Tsubasa Rise of New Champions',lien:'cracks/Captain Tsubasa Rise of New Champions/Captain Tsubasa Rise of New Champions '},
+    {name:'CarX Drift Racing',lien:'cracks/CarX Drift Racing/CarX Drift Racing '},
+    {name:'Call of Duty Black Ops II ',lien:'cracks/Call of Duty Black Ops II/Call of Duty Black Ops II '},
+    {name:'Camping Simulator',lien:'cracks/Camping Simulator/Camping Simulator '},
+    {name:'Devour',lien:'cracks/Devour/Devour '},
+    {name:'Days Gone',lien:'cracks/Days Gone/Days Gone '},
+    {name:'Farming Simulator 17',lien:'cracks/Farming Simulator 17/Farming Simulator 17 '},
+    {name:'Forts',lien:'cracks/Forts/Forts '},
+    {name:'Factorio',lien:'cracks/Factorio/factorio '},
+    {name:'Gang Beasts',lien:'cracks/Gang Beasts/Gang Beasts '},
+    {name:'Grounded',lien:'cracks/Grounded/Grounded '},
+    {name:'Ghostrunner',lien:'cracks/Ghostrunner/Ghostrunner '},
+    {name:'Hello Neighbor',lien:'cracks/Hello Neighbor/Hello Neighbor '},
+    {name:'Halo 4',lien:'cracks/Halo 4/Halo 4 '},
+    {name:'It Takes Two',lien:'cracks/It Takes Two/It Takes Two '},
+    {name:'Jump Force',lien:'cracks/Jump Force/Jump Force '},
+    {name:'Jurassic World Evolution',lien:'cracks/Jurassic World Evolution/Jurassic World Evolution '},
+    {name:'King of Seas',lien:'cracks/King of Seas/King of Seas '},
+    {name:'Levi Chronicles',lien:'cracks/Levi Chronicles/Levi Chronicles '},
+    {name:'Les Sims 4',lien:'cracks/Sims4/Sims4 '},
+    {name:'Mario Golf Super Rush',lien:'cracks/Mario Golf Super Rush/Mario Golf Super Rush '},
+    {name:'Minecraft',lien:'cracks/Minecraft/Minecraft '},
+    {name:'Naruto Shippūden : Ultimate Ninja Storm 4',lien:'cracks/NARUTO SHIPPUDEN Ultimate Ninja STORM 4/NSUNS4 '},
+    {name:'Outlast',lien:'cracks/Outlast/Outlast '},
+    {name:'Papers, Please',lien:'cracks/Paper%20Please/Paper%20Please '},
+    {name:'Phasmophobia',lien:'cracks/Phasmophobia/Phasmophobia '},
+    {name:'Planet Coaster',lien:'cracks/Planet Coaster/Planet Coaster '},
+    {name:'Planet Centauri',lien:'cracks/Planet Centauri/Planet Centauri '},
+    {name:'Prison Architect',lien:'cracks/Prison Architect/Prison Architect '},
+    {name:'Rust',lien:'cracks/Rust/Rust '},
+    {name:'Raft',lien:'cracks/Raft/Raft '},
+    {name:'Soundpad',lien:'cracks/Soundpad/Soundpad '},
+    {name:'SimCasino',lien:'cracks/SimCasino/SimCasino '},
+    {name:'Stardew Valley',lien:'cracks/Stardew Valley/Stardew Valley '},
+    {name:'Stellaris',lien:'cracks/Stellaris/Stellaris '},
+    {name:'Five Nights at Freddy\'s 1',lien:'cracks/Five%20Nights%20at%20Freddy\’s/Five%20Nights%20at%20Freddy\’s '},
+    {name:'Five Nights at Freddy\'s 2',lien:'cracks/Five%20Nights%20at%20Freddy\’s%202/Five%20Nights%20at%20Freddy\’s%202 '},
+    {name:'Five Nights at Freddy\'s 3',lien:'cracks/Five%20Nights%20at%20Freddy\’s%203/Five%20Nights%20at%20Freddy\’s%203 '},
+    {name:'Five Nights at Freddy\'s 4',lien:'cracks/Five%20Nights%20at%20Freddy\’s%204/Five%20Nights%20at%20Freddy\’s%204 '},
+    {name:'Five Nights at Freddy\'s Sister Location',lien:'cracks/Five%20Nights%20at%20Freddy\’s%20Sister%20Location/Five%20Nights%20at%20Freddy\’s%20Sister%20Location '},
+    {name:'Five Nights at Freddy\’s Help Wanted',lien:'cracks/Five%20Nights%20at%20Freddy’s%20Sister%20Help%20Wanted/Five%20Nights%20at%20Freddy’s%20Help%20Wanted '},
+    {name:'The forest',lien:'cracks/The%20Forest/The%20Forest '},
+    {name:'Adobe Illustrator 2020',lien:'cracks/Adobe%20Illustrator%202020/Adobe%20Illustrator%202020 '},
+    {name:'Control',lien:'cracks/Control/control '},
+    {name:'Totally Reliable Delivery',lien:'cracks/Totally%20Reliable%20Delivery%20Service/Totally%20Reliable%20Delivery%20Service '},
+    {name:'Dragon Ball Z Kakarot',lien:'cracks/DRAGON%20BALL%20Z%20KAKAROT/DRAGON%20BALL%20Z%20KAKAROT '},
+    {name:'Dead by Daylight + Online',lien:'cracks/DEAD%20BY%20DAYLIGHT/DEAD%20BY%20DAYLIGHT '},
+    {name:'Detroit Become Human',lien:'cracks/DETROIT%20BECOME%20HUMAN/DETROIT%20BECOME%20HUMAN '},
+    {name:'Dark Souls Remastered',lien:'cracks/DARK%20SOULS%20REMASTERED/DARK%20SOULS%20REMASTERED '},
+    {name:'Forza Horizon 4',lien:'cracks/FORZA%20HORIZON%204/FORZA%20HORIZON%204 '},
+    {name:'Grand Theft Auto V',lien:'cracks/GRAND%20THEFT%20AUTO%20V/GRAND%20THEFT%20AUTO%20V '},
+    {name:'Office Microsoft 2019',lien:'cracks/OFFICE%20MICROSOFT%202019/OFFICE%20MICROSOFT%202019 '},
+    {name:'Need for Speed REMASTERED',lien:'cracks/NEED%20FOR%20SPEED%20REMASTERED/NEED%20FOR%20SPEED%20REMASTERED '},
+    {name:'Pacify + Online',lien:'cracks/Pacify/Pacify '},
+    {name:'Rebel Inc Escalation',lien:'cracks/REBEL%20INC%20ESCALATION/REBEL%20INC%20ESCALATION '},
+    {name:'Resident Evil Village',lien:'cracks/RESIDENT%20EVIL%20VILLAGE/Resident%20Evil%20Village '},
+    {name:'Dinkum',lien:'cracks/Dinkum/Dinkum '},
+    {name:'Inside the Backrooms + Online',lien:'cracks/Inside%20the%20Backrooms/Inside%20the%20Backrooms '},
+    {name:'Youtubers Life 2',lien:'cracks/Youtubers%20Life%202/Youtubers%20Life%202 '},
+    {name:'Youtubers Life',lien:'cracks/Youtubers%20Life/Youtubers%20Life '},
+    {name:'Scrap Mechanic',lien:'cracks/Scrap%20Mechanic/Scrap%20Mechanic'},
+    {name:'Super Meat Boy Forever',lien:'cracks/Super%20Meat%20Boy%20Forever/Super%20Meat%20Boy%20Forever'},
+    {name:'Terraria',lien:'cracks/Terraria/Terraria'},
+    {name:'The Escapists 2',lien:'cracks/The%20Escapists%202/The%20Escapists%202'},
+    {name:'Undertale',lien:'cracks/Undertale/Undertale '},
+    {name:'We Are Football',lien:'cracks/We%20Are%20Football/We%20Are%20Football'},
+    {name:'60 Seconds Reatomized',lien:'cracks/60%20Seconds%20Reatomized/60%20Seconds%20Reatomized'},
+    {name:'Slime Rancher',lien:'cracks/Slime%20Rancher/Slime%20Rancher'},
+    {name:'Demon Slayer Kimetsu no Yaiba',lien:'cracks/Demon%20Slayer/Demon%20Slayer'},
+    {name:'Marvels Spider Man Remastered',lien:'cracks/Marvels%20Spider%20Man%20Remastered/Marvels%20Spider%20Man%20Remastered'},
+    {name:'Subnautica',lien:'cracks/Subnautica/Subnautica'},
+    {name:'Adobe Photoshop 2020',lien:'cracks/Adobe%20Photoshop%202020/Adobe%20Photoshop%202020'},
+    {name:'Saints Row 2022',lien:'cracks/Saints%20Row%202022/Saints%20Row%202022'},
+    {name:'Minecraft Dungeons',lien:'cracks/Minecraft%20Dungeons/Minecraft%20Dungeons'},
+    {name:'Euro Truck Simulator 2',lien:'cracks/Euro%20Truck%20Simulator%202/Euro%20Truck%20Simulator%202'},
+    {name:'Forza Horizon 5',lien:'cracks/Forza%20Horizon%205/Forza%20Horizon%205'},
+    {name:'Escape the Backrooms + Online',lien:'cracks/Escape%20the%20Backrooms/Escape%20the%20Backrooms'},
+    {name:'Life Is Strange Before the Storm Remastered',lien:'cracks/Life%20Is%20Strange%20Remastered/Life%20Is%20Strange%20Remastered'},
+    {name:'Dragon Ball Xenoverse 2',lien:'cracks/Dragon%20Ball%20Xenoverse%202/Dragon%20Ball%20Xenoverse%202'}
+];
+
+const searchInput = document.getElementById('searchInput');
+
+searchInput.addEventListener('keyup', function(){
+    const input = searchInput.value;
+    
+    const result = persons.filter(item => item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()));
+
+    let suggestion = '';
+
+    if(input.length >2){
+        if(input !=''){
+            result.forEach(resultItem => 
+                suggestion +=`
+                <a href="${resultItem.lien}"><div class="suggestion">${resultItem.name}</a></div>`
+                
+                )
+                
+            }
+
+        }
+
+    
+
+    
+
+    document.getElementById('suggestions').innerHTML = suggestion;
+
+
+
+})
+
+
+
